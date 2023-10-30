e6715847d7c657fe06396f468f474958573f3e87
Jaqueline Abreu <59976892+jaquelineabreu@users.noreply.github.com>
2023-10-30 14:43:53 -0300

    feat: Schedule a method to run periodically

--- cron-jobs/main.go
+++ cron-jobs/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/robfig/cron"
+)
+
+func main() {
+	simpleCron()
+	listen()
+
+}
+
+// Schedule a method to run periodically
+func simpleCron() {
+	fmt.Println(time.Now().String() + "Start App - Version SimpleCron")
+	c := cron.New()
+	c.AddFunc("@every 1m10s", task)
+	go c.Start()
+}
+
+func task() {
+	fmt.Println(time.Now().String() + " - Start Task")
+	time.Sleep(1 * time.Second)
+	fmt.Println(time.Now().String() + " - End Task")
+}
+
+// channel that will be notified if any system interruption signal occurs
+func listen() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	<-sig
+	fmt.Println(time.Now().String() + " - closed")
 }
