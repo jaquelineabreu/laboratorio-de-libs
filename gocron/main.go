7627dab60d1bd11725c92e309e459dc6a88c0456
Jaqueline Abreu <59976892+jaquelineabreu@users.noreply.github.com>
2023-10-30 23:41:40 -0300

    feat: study the completed gocron package

--- gocron/main.go
+++ gocron/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/go-co-op/gocron"
+)
+
+func hello(name string) {
+	message := fmt.Sprintf("Hi, %v", name)
+	fmt.Println(message)
+}
+
+func runCronJobs() {
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Every(1).Seconds().Do(func() {
+		hello("Cron Jobs")
+	})
+
+	s.StartBlocking()
+}
+
+func main() {
+	runCronJobs()
 }
